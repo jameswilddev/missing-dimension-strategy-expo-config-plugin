ad8d1d5f5c2d18c32a72382310b02a13879f03c0
jameswilddev <jameswilddev@outlook.com>
2022-10-12 10:52:24 +0100

    Hopefully fix Gradle generation.

--- app.plugin.ts
+++ app.plugin.ts
@@ -4,11 +4,14 @@ const { createBuildGradlePropsConfigPlugin } = AndroidConfig.BuildProperties;
 
 type Config = Record<string, string>;
 
+const escape = (input: string): string => `'${input.replace(/'/g, "\\'")}'`;
+
 const configPlugin: ConfigPlugin<Config> = (config, props) =>
   createBuildGradlePropsConfigPlugin<Config>(
     Object.keys(props).map((key) => ({
       propName: "android.missingDimensionStrategy",
-      propValueGetter: (config) => config[key],
+      propValueGetter: (config) =>
+        `${escape(key)}, ${escape(config[key] ?? "")}`,
     }))
   )(config, props);
 
